73b556c1ac38594902c17d330033cca2454b861c
sarperez <sarperez@student.42.fr>
2023-12-03 17:45:48 +0100

    itteri arreglado

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: sarperez <sarperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:47:54 by sarperez          #+#    #+#             */
-/*   Updated: 2023/12/03 17:37:07 by sarperez         ###   ########.fr       */
+/*   Updated: 2023/12/03 17:45:37 by sarperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		i++;
 	}
 }
-
+/* 
 void	funcion(unsigned int a, char *c)
 {
 	*c = a + *c;
@@ -39,4 +39,4 @@ int	main(void)
 
 	printf("Mapped String: %s\n", s);
 	return (0);
-}
+} */
